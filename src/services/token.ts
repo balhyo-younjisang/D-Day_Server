4421a50d87fa4ab0158d5d55405bfc917ea15e69
Lee-jeong-wook <b01024134020@gmail.com>
2024-01-27 23:23:42 +0900

    OAuth 토큰 추가

--- src/services/token.ts
+++ src/services/token.ts
@@ -34,7 +34,23 @@ export default class TokenService {
             profile: data.profile,
             name : data.name,
         }, process.env.JWT_SECRET, {
-            expiresIn: '1m',
+            expiresIn: '3h',
+            issuer: '토큰 발급자'
+        });
+        return token;
+    } catch (e) {
+      throw e;
+    }
+  }
+  public async OAuthLoginToken(data): Promise<String> {
+    try {
+        const token = jwt.sign({
+            id : data.id, 
+            profile: data.profile,
+            name : data.name,
+            email: data.email
+        }, process.env.JWT_SECRET, {
+            expiresIn: '3h',
             issuer: '토큰 발급자'
         });
         return token;
